60ab091f1c747649f011925d133ee30a1b12c50c
Semen Panfilov <whoasup@gmail.com>
2020-07-25 18:43:23 +0300

    Собрал иконки в спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const svgstore = require("gulp-svgstore");
+const rename = require("gulp-rename");
 
 // Styles
 
@@ -22,6 +24,19 @@ const styles = () => {
 }
 
 exports.styles = styles;
+
+// sprite
+
+const sprite = () => {
+  return gulp.src("source/img/icon*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest("source/img/"));
+}
+
+exports.sprite = sprite;
 
 // Server
 
